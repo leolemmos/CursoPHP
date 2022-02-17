07f0e7faeda86e409fa87748786bfda095839385
Leonardo Lemos <66037561+leolemmos@users.noreply.github.com>
2022-02-16 23:39:37 -0300

    Create conversoes.php

---
+++ conversoes.php
@@ -0,0 +1,9 @@
+<?php
+  $var = 100;
+  $type_casting = (bool) $var; // torna – se booleano
+  $type_casting = (int) $var; // torna – se inteiro
+  $type_casting = (float) $var; // torna – se float
+  $type_casting = (string) $var; // torna – se string
+  $type_casting = (array) $var; // torna – se array
+  echo $type_casting = (bool)$var; // 1
+?>
