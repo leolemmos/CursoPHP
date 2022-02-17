56ed99123a7d814096230491baed9e429f8a04d9
Leonardo Lemos <66037561+leolemmos@users.noreply.github.com>
2022-02-16 23:39:08 -0300

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,8 @@
+<?php
+  $php = array("Zend" => "CERTIFICAÇÃO", 6 => false);
+  echo $php["Zend"]; // CERTIFICAÇÃO
+  echo $php[6]; // 0
+
+  // Zend é nossa chave e CERTIFICAÇÃO nosso valor
+  // 6 é nossa chave e false(0) é nosso valor
+?>
