60ca1cfe13a9f9726c8a47483c2213edf37e4ac2
Leonardo Lemos <66037561+leolemmos@users.noreply.github.com>
2022-02-16 23:42:33 -0300

    Create else_if.php

---
+++ else_if.php
@@ -0,0 +1,13 @@
+<?php
+$nome = 'Till Lindemann';
+
+if($nome == 'Richard Kruspe') {
+  echo 'E ae Richard Kruspe!';
+} elseif ($nome == 'Oliver Riedel') {
+  echo 'E ae Oliver Riedel!';
+} elseif ($nome == 'Till Lindemann') {
+  echo 'E ae Till Lindemann!';
+} else {
+  echo "E ae $nome!";
+}
+?>
