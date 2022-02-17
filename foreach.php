0ca8144f1398bc3089234d2f89715738110268ff
Leonardo Lemos <66037561+leolemmos@users.noreply.github.com>
2022-02-16 23:50:44 -0300

    Create foreach.php

---
+++ foreach.php
@@ -0,0 +1,9 @@
+<?php
+  $ead = array('Aqui na DevMedia ', 'você se torna um ', 'desenvolvedor PHP');
+
+  foreach($ead as $can){
+    echo "$can";
+  }
+
+  //Aqui na DevMedia você se torna um desenvolvedor PHP
+?>
