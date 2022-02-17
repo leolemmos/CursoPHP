10524d9d4c397baddfacb92a0738ddbaa3532a98
Leonardo Lemos <66037561+leolemmos@users.noreply.github.com>
2022-02-16 23:46:37 -0300

    Create associacao_array2.php

---
+++ associacao_array2.php
@@ -0,0 +1,9 @@
+<?php
+  $estacao = array('Verao' => 'de 21 de dezembro a 21 de março',
+    'Outono' => 'de 21 de março a 21 de junho',
+    'Inverno' => 'de 21 de junho a 23 de setembro',
+    'Primavera' => 'de 23 de setembro a 21 de dezembro');
+  echo "A estação Verão foi: {$estacao['Verao']}";
+
+  // A estação Verão foi: de 21 de dezembro a 21 de março
+?>
